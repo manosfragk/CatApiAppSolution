18d6bc999951c39098fd6bface0a9771a115b4f5
manosfragk <manolisfragiadis@gmail.com>
2024-10-11 22:50:32 +0300

    Create database models.

---
+++ Models/CatEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace CatApiApp.Models
+{
+    /// <summary>
+    /// Represents the CatEntity stored in the database.
+    /// </summary>
+    public class CatEntity
+    {
+        public int Id { get; set; }
+        public string CatId { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Image { get; set; } // Image data storage (could be a URL or binary data)
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public List<TagEntity> Tags { get; set; } = [];
+    }
+}
---
+++ Models/TagEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace CatApiApp.Models
+{
+    /// <summary>
+    /// Represents the TagEntity related to a cat's temperament or breed.
+    /// </summary>
+    public class TagEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public List<CatEntity> Cats { get; set; } = [];
+    }
+}
