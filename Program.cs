acdb01d550d17616d76652e826622f93bdf1f7dc
manosfragk <manolisfragiadis@gmail.com>
2024-10-11 22:58:21 +0300

    Configure database connection.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using CatApiApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
