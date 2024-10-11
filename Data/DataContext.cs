efb9eb1b6504b1782e42b75cab68e5a90a6de364
manosfragk <manolisfragiadis@gmail.com>
2024-10-11 22:54:19 +0300

    Set up database context.

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using CatApiApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatApiApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<CatEntity> Cats { get; set; }
+        public DbSet<TagEntity> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Many-to-many relationship between Cats and Tags
+            modelBuilder.Entity<CatEntity>()
+                .HasMany(c => c.Tags)
+                .WithMany(t => t.Cats);
+        }
+    }
+}
