2b6456acf938f3ba1741c59333aa283455afefdd
manosfragk <manolisfragiadis@gmail.com>
2024-10-12 12:11:08 +0300

    Add API key to configuration and update Program.cs to use it.

--- Program.cs
+++ Program.cs
@@ -5,15 +5,30 @@ using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Set database
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Read CatApi settings from configuration
+var catApiSettings = builder.Configuration.GetSection("CatApi");
+var apiKey = catApiSettings["ApiKey"];
+var baseUrl = catApiSettings["BaseUrl"];
+
+// Configure Refit to use the API key for all requests
 builder.Services.AddRefitClient<ICatApiClient>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://api.thecatapi.com/"));
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri(baseUrl);
+        c.DefaultRequestHeaders.Add("x-api-key", apiKey);
+    });
+
+// Register CatService in DI container
+builder.Services.AddScoped<CatService>();
 
 var app = builder.Build();
 
