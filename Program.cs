96a5a7903cad15b6cc00d1d31a813c6311769b34
manosfragk <manolisfragiadis@gmail.com>
2024-10-13 20:06:46 +0300

    Ignore circular references, which will prevent the serializer from trying to traverse both sides of the relationship.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ using Refit;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
